a08a6481bd795cd72b0e69c0fbf11667e64a780e
Rudra Tiwari <tiwarirudra2006@gmail.com>
2025-10-03 20:22:43 +1000

    Configure Unsplash as remote image domain

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com", "nsjrzxbtxsqmsdgevszv.supabase.co"],
+    domains: [
+      "lh3.googleusercontent.com", 
+      "nsjrzxbtxsqmsdgevszv.supabase.co",
+      "images.unsplash.com" // Added for placeholder images
+    ],
   },
 };
 
