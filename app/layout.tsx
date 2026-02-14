0cc729e31a143e89646de150006a422b616937d5
Michael Ren <37400755+micha31r@users.noreply.github.com>
2026-02-14 19:25:55 +1100

    fix: opengraph image loading error

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { AuthInitializer } from "@/components/auth/AuthInitializer";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
+const defaultUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL
+  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
   : "http://localhost:3000";
 
 export const metadata: Metadata = {
