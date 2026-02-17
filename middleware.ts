4ef93e489b22be426646ac9b349a0e58fddf2465
Michael Ren <37400755+micha31r@users.noreply.github.com>
2026-02-17 22:24:14 +1100

    feat: temporarily redirect users to the waitlist

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { updateSession } from "@/lib/supabase/middleware";
-import { type NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 
-export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+// export async function middleware(request: NextRequest) {
+//   return await updateSession(request);
+// }
+
+export function middleware(request: NextRequest) {
+  return NextResponse.redirect(
+    'https://waitlist.connect3.app/',
+    302
+  )
 }
 
 export const config = {
