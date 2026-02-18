ab576c61a90e0755e0bc1d1e342748dcb8978aaf
Michael Ren <37400755+micha31r@users.noreply.github.com>
2026-02-18 20:41:28 +1100

    fix: disable humanitix onboarding

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { useAuthStore } from "@/stores/authStore";
 import { CubeLoader } from "@/components/ui/CubeLoader";
 import { ProfilePageContent } from "./ProfilePageContent";
-import SetupEventsModal from "@/components/profile/SetupEventsModal";
+// import SetupEventsModal from "@/components/profile/SetupEventsModal";
 import Sidebar from "@/components/sidebar/Sidebar";
 import { toast } from "sonner";
 
@@ -12,69 +12,69 @@ export default function ProfilePage() {
   const [editingProfile, setEditingProfile] = useState(false);
   const { user, profile, updateProfile, loading, makeAuthenticatedRequest } =
     useAuthStore();
-  const [showEventOnboardingModal, setShowEventOnboardingModal] =
-    useState(false);
+  // const [showEventOnboardingModal, setShowEventOnboardingModal] =
+  //   useState(false);
 
   useEffect(() => {
     if (loading) return;
     if (!profile) return;
 
-    if (!profile.humanitix_event_integration_setup) {
-      setShowEventOnboardingModal(true);
-    }
+    // if (!profile.humanitix_event_integration_setup) {
+    //   setShowEventOnboardingModal(true);
+    // }
   }, [loading, profile]);
 
-  const onEventModalClose = () => {
-    setShowEventOnboardingModal(false);
-  };
+  // const onEventModalClose = () => {
+  //   setShowEventOnboardingModal(false);
+  // };
 
-  const onEventModalSubmit = async (apiKey: string) => {
-    if (!profile) return;
+  // const onEventModalSubmit = async (apiKey: string) => {
+  //   if (!profile) return;
 
-    try {
-      const response = await makeAuthenticatedRequest(
-        "/api/onboarding/humanitix/createSecret",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            key: profile.id,
-            value: apiKey,
-          }),
-        },
-      );
+  //   try {
+  //     const response = await makeAuthenticatedRequest(
+  //       "/api/onboarding/humanitix/createSecret",
+  //       {
+  //         method: "POST",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //         body: JSON.stringify({
+  //           key: profile.id,
+  //           value: apiKey,
+  //         }),
+  //       },
+  //     );
 
-      if (response.ok) {
-        const responseData = await response.json();
-        let message = "API key added successfully";
-        if (responseData.success) {
-          // On success set the humanitix flag to true so we don't get the modal again
-          await updateProfile({
-            ...profile,
-            humanitix_event_integration_setup: true,
-          });
-        } else {
-          message = "Failed to add API key: " + responseData.error;
-        }
-        toast(message);
-      } else {
-        const errorData = await response
-          .json()
-          .catch(() => ({ error: "Unknown error occurred" }));
-        toast("Failed to add API key: " + errorData.error || "Request failed");
-      }
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        toast("Error adding API key: " + error.message);
-      } else {
-        toast("Error adding API key: Unknown error occurred");
-      }
-    }
+  //     if (response.ok) {
+  //       const responseData = await response.json();
+  //       let message = "API key added successfully";
+  //       if (responseData.success) {
+  //         // On success set the humanitix flag to true so we don't get the modal again
+  //         await updateProfile({
+  //           ...profile,
+  //           humanitix_event_integration_setup: true,
+  //         });
+  //       } else {
+  //         message = "Failed to add API key: " + responseData.error;
+  //       }
+  //       toast(message);
+  //     } else {
+  //       const errorData = await response
+  //         .json()
+  //         .catch(() => ({ error: "Unknown error occurred" }));
+  //       toast("Failed to add API key: " + errorData.error || "Request failed");
+  //     }
+  //   } catch (error: unknown) {
+  //     if (error instanceof Error) {
+  //       toast("Error adding API key: " + error.message);
+  //     } else {
+  //       toast("Error adding API key: Unknown error occurred");
+  //     }
+  //   }
 
-    setShowEventOnboardingModal(false);
-  };
+  //   setShowEventOnboardingModal(false);
+  // };
 
   if (loading) {
     return (
@@ -100,11 +100,12 @@ export default function ProfilePage() {
       <div className="flex relative z-10">
         <Sidebar open={sidebarOpen} onOpenChange={setSidebarOpen} />
         <main className="flex-1 pt-16 md:pt-0 relative">
-          <SetupEventsModal
+          {/* Disable for now */}
+          {/* <SetupEventsModal
             isOpen={showEventOnboardingModal}
             onClose={onEventModalClose}
             onSubmit={onEventModalSubmit}
-          />
+          /> */}
           <ProfilePageContent
             editingProfile={editingProfile}
             setEditingProfile={setEditingProfile}
