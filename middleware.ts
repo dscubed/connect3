eebae8189601d2e0f10e575ec9f3b1357dd188a0
Michael Ren <37400755+micha31r@users.noreply.github.com>
2026-02-20 01:24:26 +1100

    feat: enable app

--- middleware.ts
+++ middleware.ts
@@ -2,18 +2,18 @@
 import { updateSession } from "@/lib/supabase/middleware";
 import { NextResponse, type NextRequest } from "next/server";
 
-// export async function middleware(request: NextRequest) {
-//   return await updateSession(request);
-// }
-
-export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === "/") {
-    return NextResponse.redirect(
-      'https://waitlist.connect3.app/',
-      302
-    )
-  }
+export async function middleware(request: NextRequest) {
+  return await updateSession(request);
 }
+
+// export function middleware(request: NextRequest) {
+//   if (request.nextUrl.pathname === "/") {
+//     return NextResponse.redirect(
+//       'https://waitlist.connect3.app/',
+//       302
+//     )
+//   }
+// }
 
 export const config = {
   matcher: [
