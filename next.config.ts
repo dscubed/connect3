705844f63bc400ac3e32422a73b9845c1c7b4d92
tchanwangsa <141188429+Tchanwangsa@users.noreply.github.com>
2026-02-22 11:30:36 +1100

    fix: disable image optimization due to limits

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
@@ -22,7 +23,7 @@ const nextConfig: NextConfig = {
     ],
   },
   compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
+    removeConsole: process.env.NODE_ENV === "production",
   },
 };
 
