353218f0d558e622847a4730e00bc3ba385707ca
Michael Ren <37400755+micha31r@users.noreply.github.com>
2026-02-23 23:07:12 +1100

    fix: update uni selection UI

--- components/ui/dropdown-menu.tsx
+++ components/ui/dropdown-menu.tsx
@@ -100,15 +100,15 @@ const DropdownMenuCheckboxItem = React.forwardRef<
   <DropdownMenuPrimitive.CheckboxItem
     ref={ref}
     className={cn(
-      "relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+      "group/checkbox relative flex cursor-pointer select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
       className,
     )}
     checked={checked}
     {...props}
   >
-    <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
+    <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center rounded border border-gray-300 group-data-[state=checked]/checkbox:border-violet-500 group-data-[state=checked]/checkbox:bg-violet-500">
       <DropdownMenuPrimitive.ItemIndicator>
-        <Check className="h-4 w-4" />
+        <Check className="h-2.5 w-2.5 text-white" />
       </DropdownMenuPrimitive.ItemIndicator>
     </span>
     {children}
